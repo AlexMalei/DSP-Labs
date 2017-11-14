a042cf911684ff7d0d7f37ba1fcf6daa17c42e6c
AlexMalei <alexander.malei@gmail.com>
2017-11-14 15:31:30 +0300

    add task4 fo 1lab

--- Laba1/src/sample/Controller.java
+++ Laba1/src/sample/Controller.java
@@ -26,6 +26,9 @@ public class Controller {
     private LineChart chart3;
 
     @FXML
+    private LineChart chart4;
+
+    @FXML
     private ToggleGroup phase1;
 
     @FXML
@@ -45,6 +48,9 @@ public class Controller {
 
     @FXML
     private ToggleGroup n4;
+
+    @FXML
+    private ToggleGroup n5;
 
 
     public void task2aStart() {
@@ -94,7 +100,7 @@ public class Controller {
             double funcValue = 0;
 
             for (int k = 0; k < 5; k++) {
-                funcValue += amplitude[k] + Math.sin(2 * Math.PI * frequency[k] * n / N + phase[k]);
+                funcValue += getFunctionResult(phase[k], amplitude[k], frequency[k], N, n);
             }
             series.getData().add(new XYChart.Data<>(String.valueOf(n), funcValue));
         }
@@ -103,18 +109,53 @@ public class Controller {
         setChartProperties(chart3);
     }
 
+    public void task4Start() {
+        RadioButton rbN5 = (RadioButton) n5.getSelectedToggle();
+        double[] phase = {Math.PI / 6, Math.PI / 2, Math.PI / 3, Math.PI / 9, 0};
+        double[] amplitude = {6, 6, 6, 6, 6};
+        double[] frequency = {1, 2, 3, 4, 5};
+        double N = Double.parseDouble(rbN5.getText());
+        chart4.getData().clear();
+        XYChart.Series series = new XYChart.Series();
+        double[] multipliers = {0.20 / (N / frequency[0]), 0.20 / (N / frequency[1]), 0.20 / (N / frequency[2]), 0.20 / (N / frequency[3]), 0.20 / (N / frequency[4])};
+        for (int n = 0; n <= N; n++) {
+            double funcValue = 0;
+            for (int k = 0; k < 5; k++) {
+                funcValue += getFunctionResult(phase[k], amplitude[k], frequency[k], N, n);
+            }
+            series.getData().add(new XYChart.Data<>(String.valueOf(n), funcValue));
+            incrementParams(phase, multipliers);
+            incrementParams(amplitude, multipliers);
+            incrementParams(frequency, multipliers);
+
+        }
+        series.setName("x(n) = Σ (A[j] * sin( 2 * π * f[j] * n / N + phi[j] ))");
+        chart4.getData().add(series);
+        setChartProperties(chart4);
+    }
+
+    private void incrementParams(double[] array, double[] multipliers) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] += array[i] * multipliers[i];
+        }
+    }
+
     private void performCalculation(LineChart chart, double phase, double amplitude, double frequency, double N) {
         chart.getData().clear();
         XYChart.Series series = new XYChart.Series();
 
         for (int n = 0; n <= N; n++) {
-            series.getData().add(new XYChart.Data<>(String.valueOf(n), amplitude * Math.sin(2 * Math.PI * frequency * n / N + phase)));
+            series.getData().add(new XYChart.Data<>(String.valueOf(n), getFunctionResult(phase, amplitude, frequency, N, n)));
         }
         series.setName(String.format("x(n) = %s * sin( 2 * π * %s * n / N + %s )", amplitude, frequency, phase));
         chart.getData().add(series);
         setChartProperties(chart);
     }
 
+
+    private double getFunctionResult(double phase, double amplitude, double frequency, double N, double n) {
+        return amplitude * Math.sin(2 * Math.PI * frequency * n / N + phase);
+    }
 
     private void setChartProperties(LineChart chart) {
         chart.setHorizontalGridLinesVisible(false);
